9c96bf5fffa8d68efc040139032a052a0dcf9e9c
Akumanomi <akumanomi1988@gmail.com>
2023-06-15 11:05:09 +0200

    Final

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@
 using Microsoft.CognitiveServices.Speech.Audio;
 using TextToSpeechAzure.Services;
 
-const string speechKey = "d8bbf512f59847f79e72e006f8741510";
-const string speechRegion = "westeurope";
+const string speechKey = "";
+const string speechRegion = "";
 const string voiceName = "es-MX-CarlotaNeural";
-const string readFilePath = @"C:\Users\d.mozota\Desktop\PruebaLectura.txt";
+const string readFilePath = @"C:\File.txt";
 SpeechConfig speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
 AudioConfig audioConfig = AudioConfig.FromDefaultSpeakerOutput();
 speechConfig.SpeechSynthesisVoiceName = voiceName;
